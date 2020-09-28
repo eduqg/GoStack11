d4d66469da9215ccd021f7f9324afebd25074970
eduqg <eduardoqgomes@gmail.com>
2020-09-28 14:21:28 -0300

    Moving challenge 10 crud reactjs

--- aulas/backend-gobarber/src/modules/appointments/services/ListProvidersService.ts
+++ aulas/backend-gobarber/src/modules/appointments/services/ListProvidersService.ts
@@ -21,9 +21,10 @@ class ListProvidersService {
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    let users = await this.cacheProvider.recover<User[]>(
-      `providers-list:${user_id}`,
-    );
+    let users;
+    // let users = await this.cacheProvider.recover<User[]>(
+    //   `providers-list:${user_id}`,
+    // );
 
     if (!users) {
       users = await this.usersRepository.findAllProviders({
--- aulas/gobarbermobile/src/pages/Profile/index.tsx
+++ aulas/gobarbermobile/src/pages/Profile/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View, Button } from 'react-native';
 import { useAuth } from '../../hooks/auth';
-import { Container } from './styles';
 
-const Dashboard: React.FC = () => {
+const Profile: React.FC = () => {
   const { signOut } = useAuth();
-  return <Container />;
+  return (
+    <View>
+      <Button title="Sair" onPress={signOut} />
+    </View>
+  );
 };
 
-export default Dashboard;
+export default Profile;
